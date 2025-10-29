0467b1f9e47370170a74f98885d0320be9454c1e
mariarita-xl <xaviermaria0235@gmail.com>
2025-10-28 22:18:24 -0300

    Limpando o repositório remoto

--- usuario.py
+++
@@ -1,7 +0,0 @@
-class Usuario:
-    def __init__(self, cpf, nome_completo, telefone, email, senha):
-        self.cpf = cpf
-        self.nome_completo = nome_completo
-        self.telefone = telefone
-        self.email = email
-        self.senha = senha
